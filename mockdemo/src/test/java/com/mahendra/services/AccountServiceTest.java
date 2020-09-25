a9eccfa20098418665c7164f57edb319ad68ee94
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2020-09-25 15:57:28 +0530

    shift to beforeMethod

--- mockdemo/src/test/java/com/mahendra/services/AccountServiceTest.java
+++ mockdemo/src/test/java/com/mahendra/services/AccountServiceTest.java
@@ -24,20 +24,24 @@ public class AccountServiceTest {
 	@Mock
 	private AccountDAO dao;
 
+	Account ac = null;
+
+	@Before
+	public void init() {
+		ac = new Account(101, "Xyz", 2300);
+		when(dao.save(ac)).thenReturn(true);
+		when(dao.find(101)).thenReturn(ac);
+	}
+
 	@Test
 	public void testCreate() {
-		Account ac = new Account(101, "Xyz", 2300);
-		when(dao.save(ac)).thenReturn(true);
 		service.create(ac);
-		when(dao.find(101)).thenReturn(ac);
 		Account ac2 = dao.find(101);
 		assertEquals(ac, ac2);
 	}
 
 	@Test
 	public void testFindByAccNo() {
-		Account ac = new Account(101, "Xyz", 2300);
-		when(dao.find(101)).thenReturn(ac);
 		assertEquals(ac, service.findByAccNo(101));
 	}
 
